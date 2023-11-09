33369e0aa4f4e62550631c9e509aa0bcfbe3af2f
diana cadena <diana.marcela.cadena@correounivalle.edu.co>
2023-11-08 23:44:20 -0500

    Add dependencia de animacion y fuente (mejorar practica)

--- app/src/main/java/com/example/grupodos_dam/view/fragment/SplashPicobotellaFragment.kt
+++ app/src/main/java/com/example/grupodos_dam/view/fragment/SplashPicobotellaFragment.kt
@@ -7,11 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.grupodos_dam.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashPicobotellaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashPicobotellaFragment : Fragment() {
 
     override fun onCreateView(
@@ -21,5 +16,6 @@ class SplashPicobotellaFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_picobotella, container, false)
     }
+
 
 }
