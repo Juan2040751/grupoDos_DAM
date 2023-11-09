cd5d104ba50b5d8d8dfda073d863fc2588a10d99
diana cadena <diana.marcela.cadena@correounivalle.edu.co>
2023-11-09 00:00:53 -0500

    Añadiendo la logica inicial de girar botella con el gif

--- app/src/main/java/com/example/grupodos_dam/view/fragment/HomePicobotellaFragment.kt
+++ app/src/main/java/com/example/grupodos_dam/view/fragment/HomePicobotellaFragment.kt
@@ -6,55 +6,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.grupodos_dam.R
+import android.widget.ImageView
+import android.view.animation.RotateAnimation
+import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomePicobotellaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomePicobotellaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var anguloActual: Float = 0f
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_picobotella, container, false)
+        val view = inflater.inflate(R.layout.fragment_home_picobotella, container, false)
+
+        val botella: ImageView = view.findViewById(R.id.imgBottle)
+        val gifButton: ImageView = view.findViewById(R.id.gifButton)
+
+        gifButton.setOnClickListener {
+            rotarImagen(botella)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomePicobotellaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomePicobotellaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun rotarImagen(botella: ImageView) {
+        val anguloInicio: Float = anguloActual
+        val anguloAleatorio: Float = Random.nextInt(360).toFloat()
+        val animation = RotateAnimation(
+            anguloInicio, anguloAleatorio,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+            RotateAnimation.RELATIVE_TO_SELF, 0.5f
+        )
+
+        animation.duration = 2000
+        animation.repeatCount = 0
+        anguloActual = (anguloInicio + anguloAleatorio) % 360
+        botella.clearAnimation()
+        botella.rotation = anguloActual
+        botella.startAnimation(animation)
     }
 }
