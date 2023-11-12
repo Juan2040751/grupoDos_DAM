7b5191b91b618060974015bf904fd6f56bfd0380
Cmlooz <55704152+jcortizmba@users.noreply.github.com>
2023-11-12 14:08:54 -0500

    Ajustes PB-89 Modal Agregar reto

--- app/src/main/java/com/example/grupodos_dam/webservice/ApiUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.grupodos_dam.webservice
-
-class ApiUtils {
-    companion object{
-        fun getApiService():ApiService{
-            return RetrofitClient.getRetrofit().create(ApiService::class.java)
-        }
-    }
-}
